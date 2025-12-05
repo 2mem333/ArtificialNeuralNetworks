60f5e784a90945a1c256ac2e4b6105719015857a
efe sevinc <melihefesevinc@gmail.com>
2025-12-05 14:48:41 +0300

    Multi Layer Update
    Started developing multi-layer training code. Not finished yet.

--- ArtificialNeuralNetworks/ArtificialNeuralNetworks/MultiLayerNetwork.h
+++ ArtificialNeuralNetworks/ArtificialNeuralNetworks/MultiLayerNetwork.h
@@ -1,1 +1,278 @@
 #pragma once
+#include <iostream>
+#include <math.h>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+
+//#define DEBUG
+#ifdef DEBUG
+#define DLOG(msg) std::cout << msg 
+#else
+#define DLOG(msg)
+#endif
+
+#define euler 2.718281828459045
+
+float lc = 0.01; //global var.
+
+class MultiLayerNetwork {
+public:
+
+	int layerCount; //SHOWS HOW MANY H.LAYER THERE WILL BE
+	int* LayerNeuronCounts; // ALSO HOW MUCH NEURON WILL BE USED FOR EACH LAYER.
+
+	int *LayerSizes;
+	int* LayerStartsInd; //neurons dizisi icinde her bir layerin start indeksi
+	int* LayerBStartInd;
+
+	float totalHnSize;
+	float totalBiasSize;
+
+	float* Neurons;
+	float* bias;
+
+	float* inputs;
+	int dimension;
+	int inputCount;
+	int addedInputCount = 0;
+
+	float* mean;
+	float* std;
+
+	//BOYUT, GIRIS SAYISI, ARA KATMAN NORON SAYISI, CIKIS NORON SAYISI
+	MultiLayerNetwork(int hlCount, int* hNCounts,int dim, int ic)
+	{
+		layerCount = hlCount;
+		LayerNeuronCounts = hNCounts;
+		LayerSizes = new int[hlCount];
+		LayerStartsInd = new int[hlCount];
+		LayerBStartInd = new int[hlCount];
+
+		LayerSizes[0] = dim * hNCounts[0];
+		totalHnSize = LayerSizes[0];
+		totalBiasSize = hNCounts[0];
+		LayerStartsInd[0] = 0;
+		LayerBStartInd[0] = 0;
+
+
+		for (int layer = 1; layer < layerCount; layer++)
+		{
+			LayerSizes[layer] = hNCounts[layer - 1] * hNCounts[layer];
+
+			LayerStartsInd[layer] = totalHnSize;
+			totalHnSize += LayerSizes[layer];
+
+			LayerBStartInd[layer] = totalBiasSize;
+			totalBiasSize += hNCounts[layer];
+
+		}
+
+		Neurons = new float[totalHnSize];
+		bias = new float[totalBiasSize];
+
+		inputs = new float[ic * dim];
+		dimension = dim;
+		inputCount = ic;
+		mean = new float[dim];
+		std = new float[dim];
+	}
+
+	
+	void showStatus()
+	{
+		std::cout << "Hidden layer count: " << layerCount << "\n";
+		std::cout << "Bias array size: " << totalBiasSize << "\n";
+		std::cout << "Neuron array size: " << totalHnSize << "\n\n";
+
+		for (int layer = 0; layer < layerCount; layer++)
+		{
+			std::cout << "Layer " << layer << ": " << LayerStartsInd[layer] << "   " << 
+				LayerSizes[layer] + LayerStartsInd[layer] - 1 << "\n";
+			std::cout << "Neuron Count: " << LayerNeuronCounts[layer] << "\n";
+			std::cout << "Bias start ind: " << LayerBStartInd[layer] << "\n\n";
+		}
+
+	}
+
+	//AĞIRLIKLARIN DEĞERLERİ RANDOM BAŞLATILMALI
+	//EĞER HEPSİ AYNI KALIRSA, AYNI INPUT,TUREV,GRADYANT YUZUNDEN NORONLARIN AGIRLIKLARI HEP AYNI OLUR
+	void initializeWeights()
+	{
+		for (int i = 0; i < totalHnSize; i++)
+		{
+			Neurons[i] = ((float)rand() / RAND_MAX) * 0.2f - 0.1f;
+		}
+	}
+	void initializeBias()
+	{
+		for (int i = 0; i < totalBiasSize; i++)
+		{
+			bias[i] = ((float)rand() / RAND_MAX) * 0.2f - 0.1f;
+		}
+	}
+
+
+	void readInputs(std::ifstream& file)
+	{
+		float* input = new float[dimension + 1]; //last one is for label.
+		std::string line;
+
+		while (std::getline(file, line)) {
+			std::stringstream ss(line);
+
+			for (int d = 0; d < dimension + 1; d++)
+			{
+				ss >> input[d];
+			}
+
+			addInput(input);
+		}
+		delete[] input;
+
+	}
+	void addInput(float* inp)
+	{
+		if (addedInputCount == inputCount)
+			return;
+
+		for (int i = 0; i < dimension + 1; i++)
+		{
+			inputs[i * inputCount + addedInputCount] = inp[i];
+		}
+		addedInputCount++;
+	}
+
+	void giveInputs(float* inp)
+	{
+		inputs = inp;
+	}
+	void printInputs()
+	{
+		std::cout << "-------INPUTS-----\n";
+		for (int ind = 0; ind < inputCount; ind++)
+		{
+			for (int d = 0; d < dimension + 1; d++)
+			{
+				std::cout << inputs[ind + d * inputCount] << "  ";
+			}
+			std::cout << "\n";
+		}
+		std::cout << "-------------------\n";
+	}
+
+	float* normalizeInputs()
+	{
+		float* normalized = new float[inputCount * dimension];
+
+		//CALCULATE MEAN
+
+		for (int i = 0; i < dimension; i++)
+		{
+			mean[i] = 0.0f;
+			for (int inputIndeks = 0; inputIndeks < inputCount; inputIndeks++)
+			{
+				mean[i] += inputs[inputIndeks + i * inputCount];
+			}
+			mean[i] /= inputCount;
+		}
+
+		//CALCULATE STANDART VARIATION
+		for (int i = 0; i < dimension; i++) {
+			std[i] = 0.0f;
+			for (int inputIndeks = 0; inputIndeks < inputCount; inputIndeks++) {
+				float diff = inputs[inputIndeks + i * inputCount] - mean[i];
+				std[i] += diff * diff;
+			}
+			std[i] = sqrt(std[i] / inputCount);
+			if (std[i] < 0.0001f) std[i] = 1.0f; // Sıfır bölme hatasını önle
+		}
+
+		//APPLY NORMALIZATION
+		for (int inputIndeks = 0; inputIndeks < inputCount; inputIndeks++) {
+			for (int i = 0; i < dimension; i++) {
+				normalized[inputIndeks + i * inputCount] = (inputs[inputIndeks + i * inputCount] - mean[i]) / std[i];
+			}
+		}
+
+		return normalized;
+	}
+
+	void initializeArray(float* array, int size)
+	{
+		for (int i = 0; i < size; i++)
+			array[i] = 0;
+	}
+
+	
+	int StartLearning(float minErr, float maxEpoch)
+	{
+		float* normalizedSamples = normalizeInputs();
+
+		int cycle = 0;
+		bool isFinished = false;
+		float totalErr = 0;
+
+		float* net = new float[totalBiasSize];
+		float* fnet = new float[totalBiasSize];
+		float* fnetDer = new float[totalBiasSize];
+		float* errors = new float[LayerNeuronCounts[layerCount-1]];
+
+			totalErr = 0;
+			for (int inputInd = 0; inputInd < inputCount; inputInd++)
+			{
+				
+				//FIRST LAYER FF
+				for (int ni = 0; ni < LayerNeuronCounts[0]; ni++)
+				{
+					int indeks = ni + LayerBStartInd[0]; //ilk layerin bias indeksi
+
+					net[indeks] = 0;
+					for (int d = 0; d < dimension; d++)
+						net[indeks] += normalizedSamples[inputInd + inputCount * d] * Neurons[ni + LayerNeuronCounts[0] * d];
+
+					net[indeks] += bias[indeks];
+
+
+					fnet[indeks] = ((2.0f / ((float)1.0f + exp(-net[indeks]))) - 1.0f);
+					fnetDer[indeks] = (0.5f * (1.0f - fnet[indeks] * fnet[indeks]));
+				}
+
+				//NEXT LAYERS FF
+				for (int layer = 1; layer < layerCount; layer++)
+				{
+					for (int ni = 0; ni < LayerNeuronCounts[layer]; ni++)
+					{
+						int indeks = ni + LayerBStartInd[layer];
+
+						net[indeks] = 0;
+						for (int d = 0; d < LayerNeuronCounts[layer - 1]; d++)
+							net[indeks] += Neurons[d + LayerStartsInd[layer - 1]] //onceki layerdan ciktilar
+							*
+							Neurons[ni + LayerNeuronCounts[layer - 1] * d + LayerStartsInd[layer]];//ni + dim*d + başlangıç offset
+
+						net[indeks] += bias[indeks];
+						fnet[indeks] = ((2.0f / ((float)1.0f + exp(-net[indeks]))) - 1.0f);
+						fnetDer[indeks] = (0.5f * (1.0f - fnet[indeks] * fnet[indeks]));
+					}
+				}
+
+				////CALCULATE ERROR
+				int outLayer = layerCount - 1; //select the output layer
+				for (int ni = 0; ni < LayerNeuronCounts[outLayer];ni++)
+				{
+					float desired = -1;
+					if (inputs[inputInd + dimension * inputCount] == outLayer)
+						desired = 1;
+
+					errors[ni] = desired - fnet[ni + LayerBStartInd[outLayer]];
+					totalErr += errors[ni] * errors[ni] * 0.5f;
+				}
+
+				
+			}
+			std::cout << totalErr << " ERROR\n";
+			return 0;
+	}
+};
